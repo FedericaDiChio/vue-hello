9c4a1130d7928618e99ef071bec72db65970830a
FedericaDiChio <federica.dichio7@gmail.com>
2021-07-27 15:26:15 +0200

    inizializzazione Vue

---
+++ script.js
@@ -0,0 +1,14 @@
+/* 
+TRACCIA:
+Descrizione:
+Inizializzare Vue e stampare a schermo un messaggio all’interno di un h1, 
+utilizzando le properietà di Vue presenti in data.
+*/
+
+//Facciamo un controllo con il console.log
+console.log("Test:", Vue);
+
+//Inizializziamo Vue
+const root = new Vue ({
+
+});
