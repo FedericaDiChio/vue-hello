fc09360d7510944d4b48f9c8b44d5715d9051a29
FedericaDiChio <federica.dichio7@gmail.com>
2021-07-27 15:55:34 +0200

    commento

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ TRACCIA:
 Descrizione:
 Inizializzare Vue e stampare a schermo un messaggio all’interno di un h1, 
 utilizzando le properietà di Vue presenti in data.
+Bonus
+Aggiungere alla pagina un’immagine, la cui sorgente derivi anch'essa dai data di Vue.
 */
 
 //Facciamo un controllo con il console.log
